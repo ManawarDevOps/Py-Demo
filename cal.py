64ff8a1bb1d011c69049024b235a909574178a7d
Sk Manawar Hussain <manawar.hussain@happiestminds.com>
2024-05-22 18:11:16 +0530

    first commit

---
+++ cal.py
@@ -0,0 +1,24 @@
+def addition(num1, num2)
+    add = num1 + num2
+    return add
+
+def substraction(num1, num2):
+    sub = num1 - num2
+    return sub
+
+def multiplication(num1, num2):
+    mul = num1 * num2
+    return mul
+
+##num1 = float(sys.argv[1])
+##operation = sys.argv[2]
+##num2 = float(sys.argv[3])
+
+print(addition(5,10))
+
+print(substraction(10,2))
+
+print(multiplication(10,5))
+
+
+
